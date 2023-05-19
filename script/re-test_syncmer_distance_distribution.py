0ba3deab56177671597c0c1fc2b71b8c13116a41
Shaopeng Liu <sml6467@psu.edu>
2023-05-19 13:21:56 -0400

    update test: how shift affect compression and coverage

--- script/re-test_syncmer_distance_distribution.py
+++ script/re-test_syncmer_distance_distribution.py
@@ -90,7 +90,7 @@ def local_variable():
 	svalue = 8
 	fraction_factor = 5
 	rev_comp = False
-	shift_value = 0
+	shift_high = int((kvalue - svalue) / 2)
 	genome_list = check_files(genome_list)
 	
 	
@@ -101,7 +101,6 @@ if __name__ == '__main__':
 	parser.add_argument('-f', '--input_file', type=str, help="Path to file containing genome files to check")
 	parser.add_argument('-k', '--kvalue', type=int, help="k value to use", default=12)
 	parser.add_argument('-s', '--svalue', type=int, help="s value to use", default=8)
-	parser.add_argument('-t', '--shift_value', type=int, help="shift of submer for syncmer", default=0)
 	parser.add_argument('-a', '--fraction_factor', type=int, help="FMH fraction factor, ratio is 1/r", default=5)
 	parser.add_argument('-r', '--rev_comp', type=str, help="Use canonical kmer", default='False')
 	
@@ -112,7 +111,7 @@ if __name__ == '__main__':
 	kvalue = args.kvalue
 	svalue = args.svalue
 	fraction_factor = args.fraction_factor
-	shift_value = args.shift_value
+	shift_high = int((kvalue - svalue) / 2)
 	
 	rev_comp = args.rev_comp
 	rev_comp = rev_comp == 'True'
@@ -121,34 +120,81 @@ if __name__ == '__main__':
 		
 	
 	### process file in a brute force manner
-	df_to_merge = []
+	print("Getting distance distribution......")
+	for shift_value in range(shift_high + 1):
+		print("shift value is "+str(shift_value))
+		df_to_merge = []
+		
+		for fasta in genome_list:
+			temp_seq_list = generate_string_list_from_genome(fasta)
+			# merge seqs for convenience, will introduce few extra kmers, but won't affect results as there are few contigs
+			temp_seq = "".join(temp_seq_list)
+			# record syncmer location
+			temp_location = []
+			
+			# loop through seq
+			for i in range(len(temp_seq) - kvalue + 1):
+				current_kmer = temp_seq[i:i + kvalue]
+				if is_kmer_open_syncmer_by_hash_value(current_kmer, s=svalue, shift=shift_value):
+					temp_location.append(i)
+			
+			# check distance
+			temp_location = np.array(temp_location)
+			temp_distance = temp_location[1:] - temp_location[:-1]
+			
+			# change to freq dist.
+			temp_name = os.path.basename(fasta)
+			df_to_merge.append(pd.DataFrame(pd.Series(temp_distance).value_counts(), columns=[temp_name]))
+		
+		out_df_dist_syncmer = pd.concat(df_to_merge, axis=1)
+		out_df_dist_syncmer = clean_dist_distribution_dataframe(out_df_dist_syncmer)
+		out_df_dist_syncmer.to_csv("distance_distribution_k%ds%dt%d.csv" % (kvalue, svalue, shift_value), header=True,
+		                           index=True)
 	
-	for fasta in genome_list:
-		print("Processing file " + fasta)
-		temp_seq_list = generate_string_list_from_genome(fasta)
-		print(len(temp_seq_list))
-		print([len(x) for x in temp_seq_list])
-		# merge seqs for convenience, will introduce few extra kmers, but won't affect results as there are few contigs
-		temp_seq = "".join(temp_seq_list)
-		# record syncmer location
-		temp_location = []
-		
-		# loop through seq
-		for i in range(len(temp_seq) - kvalue + 1):
-			current_kmer = temp_seq[i:i+kvalue]
-			if is_kmer_open_syncmer_by_hash_value(current_kmer, s=svalue, shift=shift_value):
-				temp_location.append(i)
-				
-		# check distance
-		temp_location = np.array(temp_location)
-		temp_distance = temp_location[1:] - temp_location[:-1]
-		
-		# change to freq dist.
-		temp_name = os.path.basename(fasta)
-		df_to_merge.append(pd.DataFrame(pd.Series(temp_distance).value_counts(), columns=[temp_name]))
-		
+
+	### added: check compression and sequence coverage change for different shift values
+	out_df_compression = []
+	out_df_coverage = []
 	
-	out_df_dist_syncmer = pd.concat(df_to_merge, axis=1)
-	out_df_dist_syncmer = clean_dist_distribution_dataframe(out_df_dist_syncmer)
-	out_df_dist_syncmer.to_csv("re-test_dist_distribution_syncmer.csv", header=True, index=True)
+	print("Getting compression and seq coverage......")
+	for shift_value in range(shift_high+1):
+		print("shift value is " + str(shift_value))
+		list_filename = []
+		list_compression = []
+		list_coverage = []
+		for fasta in genome_list:
+			# get a merged string
+			list_filename.append(os.path.basename(fasta))
+			temp_seq_list = generate_string_list_from_genome(fasta)
+			temp_seq = "".join(temp_seq_list)
+			
+			# loop through seq
+			temp_location = []
+			for i in range(len(temp_seq) - kvalue + 1):
+				current_kmer = temp_seq[i:i + kvalue]
+				if is_kmer_open_syncmer_by_hash_value(current_kmer, s=svalue, shift=shift_value):
+					temp_location.append(i)
+					
+			# compression ratio
+			temp_compression = len(temp_location) * 1.0 / (len(temp_seq)-kvalue+1)
+			list_compression.append(temp_compression)
+					
+			# get seq coverage
+			temp_seq_cov = np.zeros(len(temp_seq), dtype=int)
+			for loc in temp_location:
+				temp_seq_cov[loc:loc+kvalue] = 1
+			temp_coverage = np.sum(temp_seq_cov)*1.0 / len(temp_seq)
+			list_coverage.append(temp_coverage)
+		
+		# merge to the dict
+		out_df_compression.append(pd.DataFrame(list_compression, index=list_filename, columns=[str(shift_value)]))
+		out_df_coverage.append(pd.DataFrame(list_coverage, index=list_filename, columns=[str(shift_value)]))
+		
+	# merge df
+	merged_df_compression = pd.concat(out_df_compression, axis=1)
+	merged_df_compression.to_csv("compression_change_by_shift_k%ds%dt.csv" %(kvalue, svalue), header=True, index=True)
+	
+	merged_df_coverage = pd.concat(out_df_coverage, axis=1)
+	merged_df_coverage.to_csv("coverage_change_by_shift_k%ds%dt.csv" % (kvalue, svalue), header=True, index=True)
+			
 			
